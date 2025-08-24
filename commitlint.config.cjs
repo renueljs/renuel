c1f097a3ad62072170cc693a8e41cc35603acb20
Nick Saunders <nick@saunde.rs>
2025-08-24 09:10:53 -0700

    misc: add project files (#1)

---
+++ commitlint.config.cjs
@@ -0,0 +1,8 @@
+/* eslint-disable-next-line no-undef */
+module.exports = {
+  rules: {
+    "type-enum": [2, "always", ["fix", "feat", "BREAKING CHANGE", "misc"]],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+  },
+};
