f2b31a84f3856c7eda9cd4039955fb1dc560a113
Nick Saunders <nick@saunde.rs>
2025-09-01 11:24:24 -0700

    feat: improve type safety for children
    note: this reverts c43824a which was meant to improve jsx compatibility but affected type safety.

---
+++ src/types.spec.ts
@@ -0,0 +1,46 @@
+import type { CSSProperties, ReactNode } from "react";
+
+import { _a, component, p$ } from "./index.ts";
+
+const { Message, Message$ } = component(
+  "Message",
+  ({ children }: { children: string }) => p$(children),
+);
+
+// @ts-expect-error children defined in props object instead of positionally
+Message({ children: "Hello world" });
+
+// @ts-expect-error required children missing
+Message({});
+
+// @ts-expect-error required children missing
+Message$();
+
+const sizableMessage = component(
+  "SizableMessage",
+  (_: { size: "sm" | "lg"; emphasis?: boolean; children?: ReactNode }) => p$(),
+);
+
+// @ts-expect-error skip props factory when props are required
+// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+sizableMessage.SizableMessage$;
+
+const { SizableMessage } = sizableMessage;
+
+// @ts-expect-error missing required prop
+SizableMessage({}, "Hello world");
+
+// @ts-expect-error excess prop
+SizableMessage({ size: "sm", emphasis: true, foo: 1 }, "Hello world");
+
+const { Button$ } = component(
+  "Button",
+  ({ children }: { children: (_: { style: CSSProperties }) => ReactNode }) =>
+    children({ style: {} }),
+);
+
+// @ts-expect-error excess prop
+Button$(_a({ href: "#", asdf: 1 }));
+
+// @ts-expect-error conflicting prop
+Button$(_a({ href: "#", style: { textDecoration: "none" } }));
