1005f2e52d119188f96edd9e1c6ef0b371473b08
Nick Saunders <nick@saunde.rs>
2025-09-07 12:12:40 -0700

    misc: allow revert commit prefix

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,7 +1,11 @@
 /* eslint-disable-next-line no-undef */
 module.exports = {
   rules: {
-    "type-enum": [2, "always", ["fix", "feat", "BREAKING CHANGE", "misc"]],
+    "type-enum": [
+      2,
+      "always",
+      ["fix", "feat", "BREAKING CHANGE", "misc", "revert"],
+    ],
     "type-case": [2, "always", "lower-case"],
     "type-empty": [2, "never"],
   },
